e1bd06b7b7b94b6a849cff29b6b0e33b7efac5cf
mar1n <30738216+mar1n@users.noreply.github.com>
2019-11-05 11:50:29 +0000

    i'm trying to play with css grid, flrex.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="box">Grid</div>
+        <div className="box">can</div>
+        <div className="box">be</div>
+        <div className="box">very</div>
+        <div className="box">tricky</div>
+      </div>
     </div>
   );
 }
